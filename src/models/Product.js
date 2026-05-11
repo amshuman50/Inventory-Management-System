1729a5f35278c6bef210276680b2b19e56c08c45
amshuman50 <amshuman789@gmail.com>
2026-05-11 10:01:26 +0545

    Created Schema of Product.

---
+++ src/models/Product.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose"
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Product name is required."]
+    },
+    sku: {
+        type: String,
+        required: [true, "sku is required."]
+    },
+    category: {
+        type: String,
+        required: [true, "Product category is required."]
+    },
+    supplier: {
+        type: String,
+        required: [true, "Supplier of the product is required."]
+    },
+    price: {
+        type: Number,
+        min: [1, "Price must be greater than 1."],
+        max: [1000000, "Price must be less than 100000"]
+    },
+    stock: {
+        type: Number,
+        default: 1,
+        max: [1000, "Stock must be less than 1000."]
+    },
+    status: {
+        tupe: String,
+        default: "Active",
+        enum: ["Active", "InActive"]
+    }
+});
+
+export default mongoose.model("Product", ProductSchema)
