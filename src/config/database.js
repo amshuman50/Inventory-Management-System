e4334d7388ffbe152d517b6c7bf1d9d0ac6324e4
amshuman50 <amshuman789@gmail.com>
2026-06-12 14:44:29 +0545

    awaiting mongoDB connection.

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import config from "./config.js";
 
 function connectDB() {
-    mongoose.connect(config.mongodbUrl).then(() => {
+    await mongoose.connect(config.mongodbUrl).then(() => {
         console.log("MongoDB Connected Successfully.")
     }).catch((error) => {
         console.log(error);
